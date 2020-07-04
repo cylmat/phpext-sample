008730e116978c3bd16cc671384ed6ddfb497a3d
freeonisland <freeonisland@yahoo.fr>
2020-07-04 21:41:24 +0200

    date

--- src/Date/Index.php
+++ src/Date/Index.php
@@ -10,6 +10,10 @@ class Index
     {
         $dateStart = new \DateTime();
         $dateInterval = \DateInterval::createFromDateString('-1 day');
-        $datePeriod = new \DatePeriod($dateStart, $dateInterval, 30);
+        $datePeriod = new \DatePeriod($dateStart, $dateInterval, 3);
+
+        foreach($datePeriod as $date){
+            echo $date->format("Ymd") . "<br>";
+        }
     }
 }
