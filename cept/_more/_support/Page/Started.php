c68c4de41ac4dd87d6739e472dd17aa9398960a8
freeonisland <freeonisland@yahoo.fr>
2020-07-08 21:26:57 +0200

    codecept -being tested-...

---
+++ cept/_more/_support/Page/Started.php
@@ -0,0 +1,44 @@
+<?php
+
+class Started
+{
+    // include url of current page
+    public static $URL = 'docs/02-GettingStarted#Debugging';
+    
+    public $header = 'h1#getting-started';
+
+    /**
+     * @var \AcceptdriverTester;
+     */
+    protected $acceptdriverTester;
+
+    public function __construct(\AcceptdriverTester $I)
+    {
+        $this->acceptdriverTester = $I;
+    }
+
+    /**
+     * Declare UI map for this page here. CSS or XPath allowed.
+     * public static $usernameField = '#username';
+     * public static $formSubmitButton = "#mainForm input[type=submit]";
+     */
+
+    /**
+     * Basic route example for your current URL
+     * You can append any additional parameter to URL
+     * and use it in tests like: Page\Edit::route('/123-post');
+     */
+    public static function route($param)
+    {
+        return static::$URL.$param;
+    }
+
+    public function checkSyntax()
+    {
+        $I = $this->acceptdriverTester;
+        $I->see("Syntax"); #different with PhpBrowser (exists on html source)
+        $I->seeElement($this->header);
+        $I->dontSee("Bad text in this page");
+        $I->dontSeeElement('.error');
+    }
+}
