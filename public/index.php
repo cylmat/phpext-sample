30e9a692c01a6af39e59d560ffbc076be83e1da0
cylmat <cyril.matte@gmail.com>
2021-12-28 20:30:15 +0000

    fix index

--- public/index.php
+++ public/index.php
@@ -8,8 +8,11 @@ $dirs = new \RecursiveIteratorIterator(
     new \RecursiveDirectoryIterator(__DIR__ . '/../src'), \RecursiveIteratorIterator::LEAVES_ONLY
 );
 
-$results = [];
+/*************************************
+ * LOAD ALL DISPLAYABLE FILES 'INDEX'
+ *************************************/
 
+$results = [];
 foreach ($dirs as $file) {
     /** @var \SplFileInfo $file */
     $name = $file->getFilename();
@@ -24,14 +27,17 @@ foreach ($dirs as $file) {
     }
 }
 
+/*******************
+ * DISPLAY TEMPLATE
+ *******************/
 echo '<html><body>
 
-<center style="width:70%;margin:auto">
+<center style="width:70%; margin:auto">
 <div style="text-align:left"><h2>Phpext</h2><h3>Playground with php extensions</h3></div>';
 foreach ($results as $class => $result) {
-    echo '<fieldset style="float:left; width:20%; text-align: left"><legend>'.$class.'</legend>' .
+    echo '<fieldset style="display: block; text-align: left"><legend>'.$class.'</legend><div>' .
             join("<br/>", $result) .
-        "</fieldset>";
+        "</div></fieldset>";
 }
 echo "</center>";
 echo "</body></html>";
