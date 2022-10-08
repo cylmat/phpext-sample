94a3bf13b621d552e93e37e02fe3fe88f1cd7ad9
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2022-10-08 21:26:56 +0200

    Update bootstrap.php

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,30 +1,10 @@
 <?php
 
-namespace Phpext;
-
 ini_set('display_errors', 'on');
-error_reporting(-1);
+error_reporting(E_ALL);
 
 spl_autoload_register(function(string $classname) {
     $classfile = str_replace('\\', '/', $classname) . '.php';
     $classfile = __DIR__ . str_replace(__NAMESPACE__, '', $classfile);
-
-    if (file_exists($classfile)) {
-        require_once $classfile;
-    }
+    include_once $classfile;
 });
-
-function b($limit=0) //Backtrace
-{
-    $b = debug_backtrace(0, $limit); array_shift($b); $res = [];
-    array_walk($b, function($v, $i) use (&$res) {
-        $args = [];
-        array_walk($v['args'], function($v) use (&$args) {
-            $args[] = is_object($v) ? get_class($v) : (
-                is_array( $v ) ? '[array]' : $v
-            );
-        });
-        $res[] = $v['class'] . ':' . $v['function'] . '('. ($args ? ' '.join(',', $args).' ' : '') .')' . ':' . $v['line'];
-    }); 
-    var_dump($res); 
-}
